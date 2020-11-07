5ce5cc71fd766d349b299058f9f533cd1109e615
Alex Crocker <hello@crock.dev>
2020-11-07 11:53:16 -0500

    Added site index with dynamic list counts

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php echo "Namejet: " ?><a href="/lists/namejet"><?php count( glob( getcwd() . "/lists/namejet/*" ) )  ?></a><br />
+<?php echo "Godaddy: " ?><a href="/lists/godaddy"><?php count( glob( getcwd() . "/lists/godaddy/*" ) )  ?></a><br />
+<?php echo "Snapnames: " ?><a href="/lists/snapnames"><?php count( glob( getcwd() . "/lists/snapnames/*" ) )  ?></a><br />
